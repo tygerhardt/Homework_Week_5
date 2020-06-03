a0ebf93981e8f947d2503d4272a95adeca4a6bbe
Enron Hubbard <tygerhardt@gmail.com>
2020-06-02 18:40:02 -0700

    Did wrong homework assignment. Added new code to files.

---
+++ assets/script.js
@@ -0,0 +1,93 @@
+const m = moment();
+
+var words;
+var hourInfo;
+
+$("#currentDay").text(m.format("dddd, MMMM Do"));
+
+
+$(document).ready( function() {
+    colorChange ();
+    renderText ();
+});
+
+function colorChange () {
+    
+    var realTime = moment().hours();
+    
+
+    $(".input").each(function () {
+        var timeTest = parseInt($(this).attr("id"));
+        console.log(timeTest);
+        
+        if (realTime > timeTest) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        } else if (realTime < timeTest) {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        } else {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+    });
+}
+
+$(".saveBtn").click(function() {
+    words = $(this).siblings(".input").val();
+    console.log(words);
+    hourInfo = $(this).siblings(".hour").text();
+    console.log(hourInfo);
+    localStorage.setItem(hourInfo, JSON.stringify(words));
+    
+    colorChange ();
+    renderText ();
+})
+
+
+function renderText () {
+    var saveWords9 = JSON.parse(localStorage.getItem("9:00 am"));
+    $("#9").val("");
+    $("#9").val(saveWords9);
+    
+    var saveWords10 = JSON.parse(localStorage.getItem("10:00 am"));
+    $("#10").val("");
+    $("#10").val(saveWords10);
+    
+    var saveWords11 = JSON.parse(localStorage.getItem("11:00 am"));
+    $("#11").val("");
+    $("#11").val(saveWords11);
+    
+    var saveWords12 = JSON.parse(localStorage.getItem("12:00 pm"));
+    $("#12").val("");
+    $("#12").val(saveWords12);
+    
+    var saveWords1 = JSON.parse(localStorage.getItem("1:00 pm"));
+    $("#13").val("");
+    $("#13").val(saveWords1);
+
+    var saveWords2 = JSON.parse(localStorage.getItem("2:00 pm"));
+    $("#14").val("");
+    $("#14").val(saveWords2);
+
+    var saveWords3 = JSON.parse(localStorage.getItem("3:00 pm"));
+    $("#15").val("");
+    $("#15").val(saveWords3);
+
+    var saveWords4 = JSON.parse(localStorage.getItem("4:00 pm"));
+    $("#16").val("");
+    $("#16").val(saveWords4);
+
+    var saveWords5 = JSON.parse(localStorage.getItem("5:00 pm"));
+    $("#17").val("");
+    $("#17").val(saveWords5);
+}
+
+
+function deleteItems() {
+  localStorage.clear();
+  window.location.reload();
+}
